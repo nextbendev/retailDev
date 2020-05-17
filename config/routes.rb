d3ad944f8b398eaec71177c529fec9ba98cd69ca
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-05-17 18:11:18 -0400

    Retail first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#show'
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
